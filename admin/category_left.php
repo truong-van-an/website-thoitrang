186859932ee2fadaa9ab775de9f11df6546179a2
truong-van-an <113992829+truong-van-an@users.noreply.github.com>
2023-04-10 21:45:59 +0700

    upload

---
+++ admin/category_left.php
@@ -0,0 +1,23 @@
+<section class="admin-content">
+        <div class="admin-content-left">
+            <ul>
+                <li><a href="">Danh muc</a>
+                    <ul>
+                        <li><a href="#">Them danh muc</a></li>
+                        <li><a href="#">Danh sach danh muc</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Loai San pham</a>
+                    <ul>
+                        <li><a href="#">Them danh muc</a></li>
+                        <li><a href="#">Danh sach danh muc</a></li>
+                    </ul>
+                </li>
+                <li><a href="">San pham</a>
+                    <ul>
+                        <li><a href="#">Them danh muc</a></li>
+                        <li><a href="#">Danh sach danh muc</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
---
+++ admin/categoryadd.php
@@ -0,0 +1,17 @@
+<?php
+include "header.php";
+include "category_left.php";
+?>
+
+<div class="admin-content-right">
+            <div class="admin-content-right-cartegory">
+                <h1>Them Danh Muc</h1>
+                <form action="" method="POST">
+                    <input type="text" placeholder="Nhap ten danh muc">
+                    <button type="submit">Them</button>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
